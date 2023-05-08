e4407505470fe4273c6786041fddcf2a1f30c68f
LAPTOP-L907REGI\lasko <lasko.yaron@gmail.com>
2023-05-08 23:15:27 +0300

    delete the file in hackenrollment

--- HackEnrollment.c
+++ HackEnrollment.c
@@ -819,12 +819,14 @@ void hackEnrollment(EnrollmentSystem sys, FILE* out)
         {
             fprintf(out,"Cannot satisfy constraints for %d", sys->hackers[i]->id);
             fclose(tempEnrollment);
+            remove("temp.txt");
             return;
         }
         if(sys->hackers[i]->coursesEnrolled == 0 && sys->hackers[i]->coursesLen == 1)
         {
             fprintf(out,"Cannot satisfy constraints for %d", sys->hackers[i]->id);
             fclose(tempEnrollment);
+            remove("temp.txt");
             return;
         }
     }
